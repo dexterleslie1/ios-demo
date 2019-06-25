8069d2c001df4619a4c3b1f8d6aa9ce58d49f8c2
dexterleslie <dexterleslie@gmail.com>
2019-06-25 09:16:19 +0800

    新增iso-directory demo

---
+++ ios-directory/ios-directory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios-directory
+//
+//  Created by john on 19/6/25.
+//  Copyright © 2019年 future. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
