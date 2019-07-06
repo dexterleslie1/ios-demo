ea95a3170ced162f91435816677266c01b58cbcd
Dexter.Chan <dexterleslie@gmail.com>
2019-07-06 22:31:04 +0800

    add ios-ui-uibutton demo

---
+++ ios-ui-uibutton/ios-ui-uibutton/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ios-ui-uibutton
+//
+//  Created by john on 19/7/6.
+//  Copyright © 2019年 future. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
