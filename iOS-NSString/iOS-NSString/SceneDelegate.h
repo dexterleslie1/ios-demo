09a0e11aecc4ca007c084feb26116e1ce78f35a9
dexterleslie@gmail.com <dexterleslie@gmail.com>
2020-10-10 11:53:35 +0800

    新增iOS-NSString demo

---
+++ iOS-NSString/iOS-NSString/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  iOS-NSString
+//
+//  Created by MacOS on 2020/10/10.
+//  Copyright © 2020 YingShun International Network Co., Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ iOS-NSString/iOS-NSString/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iOS-NSString
+//
+//  Created by MacOS on 2020/10/10.
+//  Copyright © 2020 YingShun International Network Co., Limited. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    // 截取子字符串
+    NSString *sipAccountUri = @"sip:5@192.168.1.66:5060";
+    NSRange range = [sipAccountUri rangeOfString:@"@"];
+    NSString *registerUri = [sipAccountUri substringFromIndex:range.location+range.length];
+    NSAssert([registerUri isEqualToString:@"192.168.1.66:5060"], @"意料之外错误");
+}
+
+
+@end
