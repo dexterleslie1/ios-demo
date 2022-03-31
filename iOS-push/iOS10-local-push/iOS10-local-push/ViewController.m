c7eef65a9de67b652ab78702bfd6fe9ff80159c8
dexterleslie@gmail.com <dexterleslie@gmail.com>
2022-03-31 09:45:38 +0800

    iOS10-local-push demo新增不延迟弹出本地通知测试用例

--- iOS-push/iOS10-local-push/iOS10-local-push/ViewController.m
+++ iOS-push/iOS10-local-push/iOS10-local-push/ViewController.m
@@ -10,7 +10,9 @@
 #import <UserNotifications/UserNotifications.h>
 
 @interface ViewController ()
+
 @property (weak, nonatomic) IBOutlet UIButton *buttonSendNotification;
+@property (weak, nonatomic) IBOutlet UIButton *buttonSendLocalNotificationWithoutDelay;
 
 @end
 
@@ -21,6 +23,7 @@
     // Do any additional setup after loading the view.
     
     [self.buttonSendNotification addTarget:self action:@selector(onButtonSendNotificationClick:) forControlEvents:UIControlEventTouchDown];
+    [self.buttonSendLocalNotificationWithoutDelay addTarget:self action:@selector(onButtonSendLocalNotificationWithoutDelayClick:) forControlEvents:UIControlEventTouchUpInside];
 }
 
 - (void) onButtonSendNotificationClick:(UIButton *) button {
@@ -34,6 +37,26 @@
     [userNotificationCenter addNotificationRequest:request withCompletionHandler:^(NSError * _Nullable error) {
         NSLog(@"成功推送通知");
     }];
+}
+
+- (void) onButtonSendLocalNotificationWithoutDelayClick:(UIButton *) button {
+    UNUserNotificationCenter *userNotificationCenter = [UNUserNotificationCenter currentNotificationCenter];
+    UNMutableNotificationContent *content = [[UNMutableNotificationContent alloc] init];
+    content.title = @"通知标题";
+    content.body = @"通知内容";
+    content.sound = [UNNotificationSound defaultSound];
+    UNTimeIntervalNotificationTrigger *trigger = [UNTimeIntervalNotificationTrigger triggerWithTimeInterval:0.001 repeats:NO];
+    CFUUIDRef uuid_ref = CFUUIDCreate(NULL);
+    CFStringRef uuid_string_ref= CFUUIDCreateString(NULL, uuid_ref);
+    NSString *uuid = [NSString stringWithString:(__bridge NSString *)uuid_string_ref];
+    CFRelease(uuid_ref);
+    CFRelease(uuid_string_ref);
+    NSString *uuidString = [uuid lowercaseString];
+    UNNotificationRequest *request = [UNNotificationRequest requestWithIdentifier:uuidString content:content trigger:trigger];
+//    [userNotificationCenter addNotificationRequest:request withCompletionHandler:^(NSError * _Nullable error) {
+//        NSLog(@"成功推送通知");
+//    }];
+    [userNotificationCenter addNotificationRequest:request withCompletionHandler:nil];
 }
 
 @end
